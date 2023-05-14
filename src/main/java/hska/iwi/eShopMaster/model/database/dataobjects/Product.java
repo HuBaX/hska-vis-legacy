039cf5dd77cb1d441eb0ed5f21157a0479b73545
der-Sebb <44447789+der-Sebb@users.noreply.github.com>
2023-05-14 14:27:34 +0200

    Added new Product constructor

--- src/main/java/hska/iwi/eShopMaster/model/database/dataobjects/Product.java
+++ src/main/java/hska/iwi/eShopMaster/model/database/dataobjects/Product.java
@@ -1,7 +1,18 @@
 package hska.iwi.eShopMaster.model.database.dataobjects;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * This class contains details about products.
@@ -35,6 +46,37 @@ public class Product implements java.io.Serializable {
 	private String details;
 
 	public Product() {
+	}
+
+	@JsonCreator
+	public Product(@JsonProperty("CategoryID")int category, @JsonProperty("Name")String name, @JsonProperty("Price")double price, @JsonProperty("Details")String details, @JsonProperty("ID") int id) throws Exception{
+		this.name = name;
+		this.price = price;
+		this.details = details;
+		this.id = id;
+		this.category = getCategoryByID(category);
+	}
+
+	private Category getCategoryByID(int id) throws Exception{
+		String apiUrl = "http://category-service:8081/getCategory?id="+id;
+		URL url = new URL(apiUrl);
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		StringBuilder response = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			response.append(line);
+		}
+		reader.close();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(response.toString());
+		String categoryName = jsonNode.get("Name").asText();
+		Category categoryObject = new Category(categoryName);
+		return categoryObject;
 	}
 
 	public Product(String name, double price, Category category) {
