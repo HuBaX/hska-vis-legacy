c1d0b2149f551b189d5c6d6d8461324ea2aa82cc
der-Sebb <44447789+der-Sebb@users.noreply.github.com>
2023-05-23 10:41:13 +0200

    ManagerImpl clean up

--- src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
+++ src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
@@ -2,44 +2,31 @@ package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.CategoryManager;
-import hska.iwi.eShopMaster.model.database.dataAccessObjects.CategoryDAO;
 import hska.iwi.eShopMaster.model.database.dataobjects.Category;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CategoryManagerImpl implements CategoryManager{
-	private CategoryDAO helper;
 	
 	public CategoryManagerImpl() {
-		helper = new CategoryDAO();
 	}
 
 	public List<Category> getCategories() throws Exception{
 		String apiUrl = "http://category-service:8081/getCategories";
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		StringBuilder response = new StringBuilder();
-		while ((line = reader.readLine()) != null) {
-			response.append(line);
-		}
-		reader.close();
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode jsonNode = objectMapper.readTree(response.toString());
 		JsonNode productsNode = jsonNode.get("categories");
@@ -50,27 +37,22 @@ public class CategoryManagerImpl implements CategoryManager{
 	public Category getCategory(int id) throws Exception{
 		String apiUrl = "http://category-service:8081/getCategory?id="+id;
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		StringBuilder response = new StringBuilder();
-		while ((line = reader.readLine()) != null) {
-			response.append(line);
-		}
-		reader.close();
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode jsonNode = objectMapper.readTree(response.toString());
-		Category categories = objectMapper.readValue(jsonNode.toString(),  new TypeReference<Category>() {});
-		return categories;
+		Category category = this.buildCategory(response);
+		return category;
 	}
 
 	public Category getCategoryByName(String name) throws Exception{
 		String apiUrl = "http://category-service:8081/getCategory?name="+name;
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
+		
+		Category category = this.buildCategory(response);
+		return category;
+	}
 
+	private StringBuilder getResponse(URL url) throws Exception{
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 
@@ -81,10 +63,15 @@ public class CategoryManagerImpl implements CategoryManager{
 			response.append(line);
 		}
 		reader.close();
+
+		return response;
+	}
+
+	private Category buildCategory(StringBuilder response) throws Exception{
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode jsonNode = objectMapper.readTree(response.toString());
-		Category categories = objectMapper.readValue(jsonNode.toString(),  new TypeReference<Category>() {});
-		return categories;
+		Category category = objectMapper.readValue(jsonNode.toString(),  new TypeReference<Category>() {});
+		return category;
 	}
 
 	public void addCategory(String name) throws Exception{
@@ -94,19 +81,18 @@ public class CategoryManagerImpl implements CategoryManager{
 		con.setRequestProperty("Content-Type", "application/json");
 		con.setRequestProperty("Accept", "application/json");
 		con.setDoOutput(true);
-		String jsonInputString = "{\"name\":\""+name+"\"}";
+		JSONObject jo = new JSONObject();
+		jo.put("name", name);
+		String jsonInputString = jo.toString();
 		OutputStream os = con.getOutputStream();
 		byte[] input = jsonInputString.getBytes("utf-8");
 		os.write(input, 0, input.length);
-		os.flush();
-		os.close();
 		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
 		StringBuilder res = new StringBuilder();
 		String responseLine = null;
 		while((responseLine = br.readLine()) != null){
 			res.append(responseLine.trim());
 		}
-		System.out.print(res.toString());	
 	}
 
 	public void delCategory(Category cat) throws Exception{
--- src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
+++ src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ProductManagerImpl.java
@@ -2,7 +2,6 @@ package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.CategoryManager;
 import hska.iwi.eShopMaster.model.businessLogic.manager.ProductManager;
-import hska.iwi.eShopMaster.model.database.dataAccessObjects.ProductDAO;
 import hska.iwi.eShopMaster.model.database.dataobjects.Category;
 import hska.iwi.eShopMaster.model.database.dataobjects.Product;
 
@@ -21,30 +20,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ProductManagerImpl implements ProductManager {
-	private ProductDAO helper;
 	
 	public ProductManagerImpl() {
-		helper = new ProductDAO();
 	}
 
 	public List<Product> getProducts() throws Exception{
 		String apiUrl = "http://product-service:8082/getProducts";
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		StringBuilder response = new StringBuilder();
-		while ((line = reader.readLine()) != null) {
-			response.append(line);
-		}
-		reader.close();
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode jsonNode = objectMapper.readTree(response.toString());
-		JsonNode productsNode = jsonNode.get("products");
-		List<Product> products = objectMapper.readValue(productsNode.toString(), new TypeReference<List<Product>>() {});
+		List<Product> products = this.buildProductList(response);
 		return products;
 	}
 	
@@ -58,38 +43,17 @@ public class ProductManagerImpl implements ProductManager {
 			if (searchMaxPrice != null)
 			uriBuilder.addParameter("maxPrice", searchMaxPrice.toString());
 			URL url = uriBuilder.build().toURL();
+			StringBuilder response = this.getResponse(url);
 			
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-	
-			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String line;
-			StringBuilder response = new StringBuilder();
-			while ((line = reader.readLine()) != null) {
-				response.append(line);
-			}
-			reader.close();
-			ObjectMapper objectMapper = new ObjectMapper();
-			JsonNode jsonNode = objectMapper.readTree(response.toString());
-			JsonNode productsNode = jsonNode.get("products");
-			List<Product> products = objectMapper.readValue(productsNode.toString(), new TypeReference<List<Product>>() {});
+			List<Product> products = this.buildProductList(response);
 			return products;
 	}
 
 	public Product getProductById(int id) throws Exception{
 		String apiUrl = "http://product-service:8082/getProduct?id="+id;
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		StringBuilder response = new StringBuilder();
-		while ((line = reader.readLine()) != null) {
-			response.append(line);
-		}
-		reader.close();
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode jsonNode = objectMapper.readTree(response.toString());
 		return objectMapper.readValue(jsonNode.toString(), new TypeReference<Product>() {});
@@ -98,7 +62,13 @@ public class ProductManagerImpl implements ProductManager {
 	public Product getProductByName(String name) throws Exception{
 		String apiUrl = "http://product-service:8082/getProductByName?name="+name;
 		URL url = new URL(apiUrl);
+		StringBuilder response = this.getResponse(url);
 
+		List<Product> products = this.buildProductList(response);
+		return products.get(0);
+	}
+
+	private StringBuilder getResponse(URL url) throws Exception{
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 
@@ -109,11 +79,16 @@ public class ProductManagerImpl implements ProductManager {
 			response.append(line);
 		}
 		reader.close();
+
+		return response;
+	}
+
+	private List<Product> buildProductList(StringBuilder response) throws Exception{
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode jsonNode = objectMapper.readTree(response.toString());
 		JsonNode productsNode = jsonNode.get("products");
 		List<Product> products = objectMapper.readValue(productsNode.toString(), new TypeReference<List<Product>>() {});
-		return products.get(0);
+		return products;
 	}
 	
 	public int addProduct(String name, double price, int categoryId, String details) throws Exception {
@@ -123,21 +98,15 @@ public class ProductManagerImpl implements ProductManager {
 		Category category = categoryManager.getCategory(categoryId);
 		
 		if(category != null){
-			String productString;
-			if(details == null){
-				JSONObject jo = new JSONObject();
-				jo.put("name", name);
-				jo.put("price", price);
-				jo.put("categoryId", category.getId());
-				productString = jo.toString();
-			} else{
-				JSONObject jo = new JSONObject();
-				jo.put("name", name);
-				jo.put("price", price);
-				jo.put("categoryId", category.getId());
+			JSONObject jo = new JSONObject();
+			jo.put("name", name);
+			jo.put("price", price);
+			jo.put("categoryId", category.getId());
+			if(details != null){
 				jo.put("details", details);
-				productString = jo.toString();
 			}
+			String productString = jo.toString();
+			
 			URL url = new URL("http://product-service:8082/addProduct");
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("POST");
@@ -152,10 +121,8 @@ public class ProductManagerImpl implements ProductManager {
 			String responseLine = null;
 			while((responseLine = br.readLine()) != null){
 				res.append(responseLine.trim());
-			}
-			System.out.print(res.toString());    
+			}  
 			productId = this.getProductByName(name).getId();
-			System.out.println(productId);
 		}
 			 
 		return productId;
